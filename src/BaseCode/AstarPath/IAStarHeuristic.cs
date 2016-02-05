f31b98d18e3cc37ed4b3d5d5f49f4a370ace313e
Crysis168 <168915087@qq.com>
2016-02-05 10:16:32 +0800

    Revert "c9:c0:9c:e4:e2:cd:9f:49:71:12:91:54:de:c9:c9:1a"
    This reverts commit ba8d968e53b6184ede12e32508e5c381f2eb464b.

--- src/BaseCode/AstarPath/IAStarHeuristic.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface IAStarHeuristic
-{
-	int Heuristic(int x1, int y1, int x2, int y2);
-}
--- src/BaseCode/Logger/IRecord.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface IRecord 
-{
-	void Log(string msg);
-	void LogWarning(string msg);
-	void LogError(string msg);
-}
